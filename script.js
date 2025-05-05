32612cad8118480066f24d3d3d13d8fc27c37d67
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-05-05 17:45:18 +0530

    Fixed bug

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function formatTime(seconds) {
 }
   
 async function gettingsongs(){
-    let a=await fetch("http://127.0.0.1:3000/Music/");
+    let a=await fetch("http://127.0.0.1:3000/Spotify-Clone/Music/");
     let response=await a.text();
     let div = document.createElement("div");
     div.innerHTML=response;
